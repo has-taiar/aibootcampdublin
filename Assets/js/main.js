f87a3a122f21e41e58971609c52bd2b3555653d3
Has A <has@vnext.com.au>
2019-11-25 22:21:28 +1100

    updated the date and time of the event

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -95,7 +95,7 @@ function register($form) {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date('Dec 15, 2018 09:00:00').getTime();
+var countDownDate = new Date('Dec 14, 2019 09:00:00').getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
